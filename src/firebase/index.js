67dca8e343462b47704c6c8cb01f2d29035fced6
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2024-04-30 00:30:39 +0500

    added envs to firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,25 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID,
-    // measurementId: process.env.REACT_APP_MEASUREMENT_ID
-
-    apiKey: "AIzaSyCl4EKlHH-1xHjs7F7tfxgL82qXal7zaD4",
-    authDomain: "alumtec-web.firebaseapp.com",
-    projectId: "alumtec-web",
-    storageBucket: "alumtec-web.appspot.com",
-    messagingSenderId: "479067030602",
-    appId: "1:479067030602:web:37da5dc52ba504d78652b3",
-    measurementId: "G-TEB55W7VZF"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
